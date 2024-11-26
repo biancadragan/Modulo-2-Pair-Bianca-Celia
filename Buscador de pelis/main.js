68f350a09baf134be07f7b647712a2dfcc1b96aa
biancadragan <andreeabiancadragan@gmail.com>
2024-11-26 21:34:01 +0100

    Add html, css and javascript files

---
+++ Buscador de pelis/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const startBtn = document.querySelector('.js_startBtn');
+
+startBtn.addEventListener = ('click', (ev) => {
+    const inception = 'Inception';
+    const theButterFlyEffect = 'The butterfly effect';
+    const eternalSunshineOfTheSM = 'Eternal sunshine of the spotless mind';
+    const blueVelvet = 'Blue velvet';
+    const split = 'Split';
+})
+const movieTitle = document.querySelector ('.js_movieTitle');
