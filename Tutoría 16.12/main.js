15d36edc4c52385c612cc75e530e1303182b1403
biancadragan <andreeabiancadragan@gmail.com>
2024-12-17 17:05:35 +0100

    Add js parse const func

---
+++ Tutoría 16.12/main.js
@@ -0,0 +1,49 @@
+'use strict';
+
+let variableNumero = '3993';
+
+//variableNumero += 1000;
+
+console.log( parseInt(variableNumero) + 1000 );
+
+
+
+const ageInput = document.querySelector('.js_ageInput');
+
+console.log(ageInput.value + 10);
+
+
+// parseInt(string)  --> Number (sin decimales)
+// parseFloat(string)  --> Number  (puede tener decimales)
+// string(Number)  -->  string
+
+
+
+const tasks = [{}, {}, {}];  // --> string
+
+// JSON.stringify( objeto | array )  --> string
+
+const variable = '[{}, {}, {}]';
+
+// JSON.parse(string)  --> obj | array
+
+
+
+const func1 = () => {
+
+};
+
+function func2() {
+
+}
+
+
+func1(  func2  )
+
+
+Element.addEventListener('click', (ev) => {} )
+
+/*
+Function
+  Callback
+*/
---
+++ rock-paper-scissor/main.js
@@ -0,0 +1,6 @@
+const playBtn = document.querySelector(".js_playBtn");
+const selectOption = document.querySelector(".js_selectOption");
+const resultText = document.querySelector(".js_resultText");
+const userScore = document.querySelector(".js_userScore");
+const computerScore = document.querySelector(".js_computerScore");
+
